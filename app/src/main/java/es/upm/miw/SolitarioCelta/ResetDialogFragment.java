15fbe78927897879593333d23b926692a0223ad3
Fran López <fclopezjurado@gmail.com>
2016-10-25 23:44:45 +0200

    New "ResetDialogFragment" class in "es.upm.miw.SolitarioCelta" package to manage the new dialog to reset games.

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/ResetDialogFragment.java
@@ -0,0 +1,10 @@
+package es.upm.miw.SolitarioCelta;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by franlopez on 25/10/2016.
+ */
+
+public class ResetDialogFragment extends DialogFragment {
+}
