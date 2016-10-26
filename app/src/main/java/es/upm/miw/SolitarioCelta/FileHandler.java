4cc787315a57cc052db04bdc22acc4dda51af980
Fran López <fclopezjurado@gmail.com>
2016-10-27 01:14:51 +0200

    Fixes in "FileHandler" class from "es.upm.miw.SolitarioCelta" package to create a new method called "fileExists". This method will be used to know if a file to read or write was created.

--- app/src/main/java/es/upm/miw/SolitarioCelta/FileHandler.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/FileHandler.java
@@ -3,6 +3,8 @@ package es.upm.miw.SolitarioCelta;
 import android.content.Context;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -40,18 +42,27 @@ public class FileHandler {
         }
     }
 
-    public String readFile(Context context) throws IOException {
+    public String readFile(Context context) {
         String fileData = "";
         String fileLine;
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-                context.openFileInput(this.getFileName())));
-        fileLine = bufferedReader.readLine();
-        while (fileLine != null) {
-            fileData += fileLine;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    context.openFileInput(this.getFileName())));
             fileLine = bufferedReader.readLine();
+            while (fileLine != null) {
+                fileData += fileLine;
+                fileLine = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return fileData;
+    }
+
+    public boolean fileExist(Context context) {
+        File file = new File(context.getFilesDir(), this.getFileName());
+        return file.exists();
     }
 }
