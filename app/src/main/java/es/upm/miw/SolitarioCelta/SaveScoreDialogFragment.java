ec456f0aa89375ea5ad9b213b84f82aae8b947cb
Fran López <fclopezjurado@gmail.com>
2016-10-27 20:46:56 +0200

    New "SaveScoreDialogFragment" class in "es.upm.miw.SolitarioCelta" package to manage the new dialog to save scores

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/SaveScoreDialogFragment.java
@@ -0,0 +1,10 @@
+package es.upm.miw.SolitarioCelta;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by franlopez on 27/10/2016.
+ */
+
+public class SaveScoreDialogFragment extends DialogFragment {
+}
