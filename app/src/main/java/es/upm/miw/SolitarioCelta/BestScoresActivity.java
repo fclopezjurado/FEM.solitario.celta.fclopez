284c667fb80e0f36ef139febd40f82823c6663db
Fran López <fclopezjurado@gmail.com>
2016-10-30 13:20:57 +0100

    New "BestScoresActivity" class in "es.upm.miw.SolitarioCelta" package to show all stored scores

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/BestScoresActivity.java
@@ -0,0 +1,10 @@
+package es.upm.miw.SolitarioCelta;
+
+import android.app.ListActivity;
+
+/**
+ * Created by franlopez on 30/10/2016.
+ */
+
+public class BestScoresActivity extends ListActivity {
+}
