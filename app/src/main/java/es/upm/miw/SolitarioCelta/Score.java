8d26e577885eac3cc3089927f523c69df359c344
Fran López <fclopezjurado@gmail.com>
2016-10-30 13:01:11 +0100

    "Score" class implementation in "es.upm.miw.SolitarioCelta" package to manage the scores

--- app/src/main/java/es/upm/miw/SolitarioCelta/Score.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/Score.java
@@ -5,4 +5,37 @@ package es.upm.miw.SolitarioCelta;
  */
 
 public class Score {
+    private String playerName;
+    private int numberOfPieces;
+    private long currentDate;
+
+    public Score(String playerName, int numberOfPieces, long currentDate) {
+        this.setPlayerName(playerName);
+        this.setNumberOfPieces(numberOfPieces);
+        this.setCurrentDate(currentDate);
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    private void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getNumberOfPieces() {
+        return numberOfPieces;
+    }
+
+    private void setNumberOfPieces(int numberOfPieces) {
+        this.numberOfPieces = numberOfPieces;
+    }
+
+    public long getCurrentDate() {
+        return currentDate;
+    }
+
+    private void setCurrentDate(long currentDate) {
+        this.currentDate = currentDate;
+    }
 }
