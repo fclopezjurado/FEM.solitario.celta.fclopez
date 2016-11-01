a89da69c0dd6814546c3429feff1bcb5123bf652
Fran López <fclopezjurado@gmail.com>
2016-11-01 15:43:26 +0100

    Fixes in "BestScoresActivity" class from "es.upm.miw.SolitarioCelta.activities" package to add a new action bar menu to "BestScoresActivity" activity.

--- app/src/main/java/es/upm/miw/SolitarioCelta/activities/BestScoresActivity.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/activities/BestScoresActivity.java
@@ -1,12 +1,18 @@
 package es.upm.miw.SolitarioCelta.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import es.upm.miw.SolitarioCelta.R;
+import es.upm.miw.SolitarioCelta.io.FileHandler;
 import es.upm.miw.SolitarioCelta.logic.Score;
 import es.upm.miw.SolitarioCelta.adapters.ScoresAdapter;
 
@@ -19,7 +25,6 @@ public class BestScoresActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.scores);
 
         ListView scoresListView = (ListView) findViewById(R.id.scoresList);
@@ -27,5 +32,32 @@ public class BestScoresActivity extends Activity {
         ScoresAdapter adapter = new ScoresAdapter(this, R.layout.score, scores);
 
         scoresListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.best_scores_menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.deleteScores:
+                FileHandler<Score> fileHandler = new FileHandler<>(FileHandler.SCORES);
+                ListView scoresListView = (ListView) findViewById(R.id.scoresList);
+
+                fileHandler.deleteFileInfo(this, Context.MODE_PRIVATE);
+                scoresListView.setAdapter(null);
+                Toast.makeText(this, getString(R.string.toastToDeleteScores), Toast.LENGTH_SHORT
+                ).show();
+                return true;
+            default:
+                NavUtils.navigateUpFromSameTask(this);
+                break;
+        }
+
+        return true;
     }
 }
