c773c2bfd55181635c7407b7eaf861c92c2bfd9c
Fran López <fclopezjurado@gmail.com>
2016-11-01 13:17:40 +0100

    "BestScoresActivity" class implementation in "es.upm.miw.SolitarioCelta" package to create an activity to show all stored scores. This activity has already been included in "AndroidManifest.xml" file.

--- app/src/main/java/es/upm/miw/SolitarioCelta/BestScoresActivity.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/BestScoresActivity.java
@@ -1,10 +1,27 @@
 package es.upm.miw.SolitarioCelta;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by franlopez on 30/10/2016.
  */
 
-public class BestScoresActivity extends ListActivity {
+public class BestScoresActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.scores);
+
+        ListView scoresListView = (ListView) findViewById(R.id.scoresList);
+        ArrayList<Score> scores = (ArrayList<Score>) getIntent().getSerializableExtra("SCORES");
+        ScoresAdapter adapter = new ScoresAdapter(this, R.layout.score, scores);
+
+        scoresListView.setAdapter(adapter);
+    }
 }
