8c3a822ba2d84749229ce79264f650729a64a02f
Fran López <fclopezjurado@gmail.com>
2016-11-01 13:06:17 +0100

    "Game" class implementation in "es.upm.miw.SolitarioCelta" package to manage the games. Using this class, the application could store multiple games in future versions.

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/Game.java
@@ -0,0 +1,23 @@
+package es.upm.miw.SolitarioCelta;
+
+import java.io.Serializable;
+
+/**
+ * Created by franlopez on 30/10/2016.
+ */
+
+public class Game implements Serializable {
+    private String boardState;
+
+    public Game(String boardState) {
+        this.setBoardState(boardState);
+    }
+
+    public String getBoardState() {
+        return boardState;
+    }
+
+    private void setBoardState(String boardState) {
+        this.boardState = boardState;
+    }
+}
